22a8f19cffb4d65d283ed3c66351442703fe9180
Ajay Shekhawat <shekhawatajay767@gmail.com>
2022-09-30 15:52:08 +0530

    api is made

--- index.js
+++ index.js
@@ -5,10 +5,79 @@ const request = require("request-promise"); // to make api call
 const app = express();
 const PORT = 5000;
 
+// const apiKey = "4a9cf18fd79f1531bd911545c1f1ec86";
+
+const generateScraperUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json()); // middleware to parse json input
 
 app.get("/", (req, res) => {
-  res.send("sab changa si");
+  res.send("Welcome to Amazon Scraper Api");
+});
+
+// get product
+app.get("/product/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl()}}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+  const response = await request();
+});
+
+// get product review
+app.get("/product/:productId/review", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl()}}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+  const response = await request();
+});
+
+// Get product offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl()}}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get product search query
+app.get("/search/:serachQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl()}}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => {
